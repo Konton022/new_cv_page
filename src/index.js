5c8fab88b00e3a44b8b01b4b48574204dce1bff2
Konton022 <42418105+Konton022@users.noreply.github.com>
2022-04-13 00:46:48 +0500

    add bags

--- src/index.js
+++ src/index.js
@@ -1,2 +1,23 @@
-import 'normalize.css'
+import 'normalize.css';
 import './styles/main.scss';
+
+// (function (m, e, t, r, i, k, a) {
+//     m[i] =
+//         m[i] ||
+//         function () {
+//             (m[i].a = m[i].a || []).push(arguments);
+//         };
+//     m[i].l = 1 * new Date();
+//     (k = e.createElement(t)),
+//         (a = e.getElementsByTagName(t)[0]),
+//         (k.async = 1),
+//         (k.src = r),
+//         a.parentNode.insertBefore(k, a);
+// })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js', 'ym');
+
+// ym(88253339, 'init', {
+//     clickmap: true,
+//     trackLinks: true,
+//     accurateTrackBounce: true,
+//     webvisor: true,
+// });
--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,71 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const CspHtmlWebpackPlugin = require('csp-html-webpack-plugin');
 
-const devServer = (isDev) => !isDev ? {} : {
-    devServer: {
-        open: true,
-        hot: true,
-        port: 8080,
-    }
-};
+const devServer = (isDev) =>
+    !isDev
+        ? {}
+        : {
+              devServer: {
+                  open: true,
+                  hot: true,
+                  port: 8080,
+              },
+          };
 
-module.exports = ({develop}) => ({
-  mode: develop ? 'development' : 'production',
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    clean: true,
-  },
-  plugins: [
-      new HtmlWebpackPlugin({
-          template: './src/index.html'
-      }),
-      new MiniCssExtractPlugin({
-          filename: './styles/main.css'
-      })
-  ],
-  module: {
-      rules: [
-          {
-              test: /\.(?:ico|png|jpg|jpeg|svg)$/i,
-              type: 'asset/inline'
-          },
-          {
-              test: /\.html$/i,
-              loader: 'html-loader'
-          },
-          {
-              test: /\.css$/i,
-              use: [
-                MiniCssExtractPlugin.loader, 'css-loader'
-              ]
-          },
-          {
-              test: /\.scss$/i,
-              use: [
-                MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'
-              ]
-          }
-      ]
-  },
-  ...devServer(develop),
+module.exports = ({ develop }) => ({
+    mode: develop ? 'development' : 'production',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: './styles/main.css',
+        }),
+        // new CspHtmlWebpackPlugin({
+        //     'object-src': "'none'",
+        //     'base-uri': "'self'",
+        //     'script-src': [
+        //         "'unsafe-inline'",
+        //         "'self'",
+        //         "'unsafe-eval'",
+        //         "'http://mc.yandex.ru'",
+        //     ],
+        //     'worker-src': ["'self'", 'blob:'],
+        //     // 'base-uri': "'self'",
+        //     // 'object-src': "'none'",
+        //     // 'default-src': "'self'",
+        //     // 'script-src-elem': "'self'",
+        //     // 'script-src': ["'unsafe-inline'", "'self'", "'unsafe-eval'"],
+        //     // 'style-src': ["'unsafe-inline'", "'self'", "'unsafe-eval'"],
+        // }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(?:ico|png|jpg|jpeg|svg)$/i,
+                type: 'asset/inline',
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.scss$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+        ],
+    },
+    ...devServer(develop),
 });
